7c49c9ca00a860660b9d40afcff2455def5c4b83
Anuja Gunasinghe <a.r.gunasinghe@gmail.com>
2024-12-31 00:48:08 +0530

    changes

--- src/com/base/engine/Window.java
+++ src/com/base/engine/Window.java
@@ -16,4 +16,23 @@ public class Window {
             e.printStackTrace();
         }
     }
+
+    public static void render() {
+        Display.update();
+    }
+
+    // Getters and Setters
+
+    public static boolean isCloseRequested() {
+        return Display.isCloseRequested();
+    }
+    public static int getWidth() {
+        return Display.getDisplayMode().getWidth();
+    }
+    public static int getHeight() {
+        return Display.getDisplayMode().getHeight();
+    }
+    public static String getTitle() {
+        return Display.getTitle();
+    }
 }
