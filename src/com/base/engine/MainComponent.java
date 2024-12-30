72772effba2750f98482f2d86981c51e3c5b85f7
Anuja Gunasinghe <a.r.gunasinghe@gmail.com>
2024-12-31 00:42:47 +0530

    first commit

---
+++ src/com/base/engine/MainComponent.java
@@ -0,0 +1,11 @@
+package com.base.engine;
+
+public class MainComponent {
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+    public static final String TITLE = "3D Game Engine";
+
+    public static void main(String[] args) {
+        Window.createWindow(WIDTH, HEIGHT, TITLE);
+    }
+}
