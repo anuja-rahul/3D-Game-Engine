3a32388434c20676aa137885c72ef797d235da11
Anuja Gunasinghe <a.r.gunasinghe@gmail.com>
2025-01-19 19:03:08 +0530

    changes

--- src/com/base/engine/Game.java
+++ src/com/base/engine/Game.java
@@ -3,8 +3,23 @@ package com.base.engine;
 import org.lwjgl.input.Keyboard;
 
 public class Game {
-    public Game() {}
+
+    private Mesh mesh;
+
+    public Game() {
+        mesh = new Mesh();
+
+        Vertex[] data = new Vertex[] {
+                new Vertex(new Vector3f(-1, -1, 0)),
+                new Vertex(new Vector3f(0, 1, 0)),
+                new Vertex(new Vector3f(1, -1, 0))
+        };
+
+        mesh.addVertices(data);
+    }
 
     public void update() {}
-    public void render() {}
+    public void render() {
+        mesh.draw();
+    }
 }
